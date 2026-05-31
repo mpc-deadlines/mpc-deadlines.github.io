f1d39f42dc6e66e72a5f1f6ac3fab9d768017988
ajithsureshtii <ajithsureshtii@users.noreply.github.com>
2026-05-31 15:07:55 +0000

    deploy: 90f0768fcaa044c15f9678089d6194ecfab9889e

--- bot/scraper.py
+++ bot/scraper.py
@@ -7,9 +7,16 @@ logger = logging.getLogger(__name__)
 
 _HEADERS = {
     "User-Agent": (
-        "Mozilla/5.0 (compatible; MPCDeadlinesBot/1.0; "
-        "+https://mpc-deadlines.github.io)"
-    )
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/125.0.0.0 Safari/537.36"
+    ),
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "DNT": "1",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
 }
 _MAX_CHARS = 8000
 
