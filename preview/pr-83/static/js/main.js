de891adef9a18d5898e793e34732fe0619fa829d
ajithsureshtii <ajithsureshtii@users.noreply.github.com>
2026-07-13 17:22:33 +0000

    deploy: ce66156707508bf57551bf55deb776e8ec1b55bc

--- preview/pr-83/static/js/main.js
+++ preview/pr-83/static/js/main.js
@@ -6948,7 +6948,7 @@ $(function() {
   
   // WPEC 2026
   
-  var rawDeadlines = ["2026-08-26 00:00","2026-09-14 00:00"] || [];
+  var rawDeadlines = ["2026-09-14 23:59"] || [];
   if (rawDeadlines.constructor !== Array) {
     rawDeadlines = [rawDeadlines];
   }
@@ -6960,7 +6960,7 @@ $(function() {
     rawDeadline = rawDeadline.replace('%y', year).replace('%Y', year - 1);
     // adjust date according to deadline timezone
     
-    var deadline = moment.tz(rawDeadline, "Etc/GMT-12");
+    var deadline = moment.tz(rawDeadline, "Etc/GMT+12"); // Anywhere on Earth
     
 
     // post-process date
@@ -7003,50 +7003,13 @@ $(function() {
           }
         }
       }
-      $('#wpec2026-theory-pract-applied-ppml-sok-wk-0 .timer').countdown(confDeadline.toDate(), make_update_countdown_fn(confDeadline));
+      $('#wpec2026-theory-pract-applied-wk-0 .timer').countdown(confDeadline.toDate(), make_update_countdown_fn(confDeadline));
       // check if date has passed, add 'past' class to it
       if (moment() - confDeadline > 0) {
-        $('#wpec2026-theory-pract-applied-ppml-sok-wk-0').addClass('past');
+        $('#wpec2026-theory-pract-applied-wk-0').addClass('past');
       }
-      $('#wpec2026-theory-pract-applied-ppml-sok-wk-0 .deadline-time').html(confDeadline.local().format('D MMM YYYY, h:mm:ss a'));
-      deadlineByConf["wpec2026-theory-pract-applied-ppml-sok-wk-0"] = confDeadline;
-    }
-  } else {
-    // TODO: hide the conf_id ?
-  }
-  
-  //
-  
-  
-  var deadlineId = 1;
-  if (deadlineId < parsedDeadlines.length) {
-    var confDeadline = parsedDeadlines[deadlineId];
-
-    // render countdown timer
-    if (confDeadline) {
-      function make_update_countdown_fn(confDeadline) {
-        return function(event) {
-          diff = moment() - confDeadline;
-          if (diff <= 0) {
-            $(this).html(event.strftime('%D days %Hh %Mm %Ss'));
-            var daysLeft = -diff / 86400000;
-            var urgency = daysLeft < 7 ? 'urgent' : daysLeft < 30 ? 'warning' : 'ok';
-            $(this).removeClass('urgent warning ok').addClass(urgency);
-            $(this).closest('.conf')
-              .removeClass('urgency-urgent urgency-warning urgency-ok')
-              .addClass('urgency-' + urgency);
-          } else {
-            $(this).html(confDeadline.fromNow());
-          }
-        }
-      }
-      $('#wpec2026-theory-pract-applied-ppml-sok-wk-1 .timer').countdown(confDeadline.toDate(), make_update_countdown_fn(confDeadline));
-      // check if date has passed, add 'past' class to it
-      if (moment() - confDeadline > 0) {
-        $('#wpec2026-theory-pract-applied-ppml-sok-wk-1').addClass('past');
-      }
-      $('#wpec2026-theory-pract-applied-ppml-sok-wk-1 .deadline-time').html(confDeadline.local().format('D MMM YYYY, h:mm:ss a'));
-      deadlineByConf["wpec2026-theory-pract-applied-ppml-sok-wk-1"] = confDeadline;
+      $('#wpec2026-theory-pract-applied-wk-0 .deadline-time').html(confDeadline.local().format('D MMM YYYY, h:mm:ss a'));
+      deadlineByConf["wpec2026-theory-pract-applied-wk-0"] = confDeadline;
     }
   } else {
     // TODO: hide the conf_id ?
